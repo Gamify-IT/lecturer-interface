642c067637b5192a32d7b7e12c64300f230dbe7c
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-08-29 19:38:02 +0200

    add licence notice link

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,30 @@ import CourseSidebarMenu from "@/components/CourseSidebarMenu.vue";
     <!-- component matched by the route will render here -->
     <div class="router-view-wrapper">
       <router-view></router-view>
+      <footer class="py-2 footer">
+        <div class="notice-right">
+          <a href="/third-party-licence-notice">Licence notice</a>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
+<style scoped>
+.footer {
+  position: fixed;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+  color: maroon;
+  text-align: center;
+  background-color: #1e1e21;
+}
+.notice-right {
+  text-align: right;
+  margin-right: 25px;
+}
+a {
+  color: rgb(118, 66, 177);
+  text-decoration: none;
+}
+</style>
