0f45c9c2861e57fd1291c422482142c2c5a7c882
Levi <otterbach.levi@gmail.com>
2022-08-30 14:41:48 +0200

    add finitequiz base url

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@ export default {
   // URL of the RESTful API
   apiBaseUrl: "/overworld/api/v1",
   apiChickenshockUrl: "/minigames/chickenshock/api/v1",
+  apiFinitequizUrl: "/minigames/finitequiz/api/v1",
 };
