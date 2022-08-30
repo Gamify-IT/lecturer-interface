77cb2116bd6b45ba2890419c6f919fe5f247c1b5
Max <39833217+maexled@users.noreply.github.com>
2022-08-30 17:19:43 +0200

    Update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@ export default {
   apiBaseUrl: "/overworld/api/v1",
   apiChickenshockUrl: "/minigames/chickenshock/api/v1",
   apiFinitequizUrl: "/minigames/finitequiz/api/v1",
+  mapCommitHash: "3616da6c60dcbb116e91d08c59d79b200b3b91a6",
 };
