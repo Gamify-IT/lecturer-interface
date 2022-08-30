4ab3ce6520016036cfc098baca97a1dc0b3ddfc5
AaronSchimd <st166450@stud.uni-stuttgart.de>
2022-08-30 18:23:14 +0200

    fix finitequiz modal

--- src/components/EditFinitequizConfigurationModal.vue
+++ src/components/EditFinitequizConfigurationModal.vue
@@ -94,6 +94,7 @@ function resetModal() {
       });
     oldMinigame.value = minigame.value;
   } else {
+    configuration.value.id = undefined;
     configuration.value.questions = [];
     oldMinigame.value = minigame.value;
   }
@@ -125,7 +126,6 @@ function handleSubmit() {
   if (!checkFormValidity()) {
     return;
   }
-  resetModal();
   emit("updateMinigameConfiguration", minigame.value);
 }
 
