13d8f0f2e4b61f6927acf67bd038412aa1b4ed6a
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-09-02 16:38:14 +0200

    fix license typo

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@ function returnFocus() {
       ></router-view>
       <footer class="py-2 footer">
         <div class="notice-right">
-          <a href="/third-party-license-notice">Licence notice</a>
+          <a href="/third-party-license-notice">License notice</a>
         </div>
       </footer>
     </div>
