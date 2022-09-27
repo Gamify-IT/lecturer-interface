ed7ebeaeb1c56f1f10240dea306f7f55ae2141f2
Ilijaz Mehmedovic <mehmedovic.ilijaz@gmail.com>
2022-09-27 15:37:04 +0200

    add bugfinder url in config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ export default {
   // URL of the RESTful API
   overworldApiUrl: "/overworld/api/v1",
   chickenshockApiUrl: "/minigames/chickenshock/api/v1",
+  bugfinderApiUrl: "/minigames/bugfinder/api/v1",
   finitequizApiUrl: "/minigames/finitequiz/api/v1",
   crosswordpuzzleApiUrl: "/minigames/crosswordpuzzle/api/v1",
   mapCommitHash: "3616da6c60dcbb116e91d08c59d79b200b3b91a6",
