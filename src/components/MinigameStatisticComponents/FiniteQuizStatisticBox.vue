240e9a43d6296b4aebb0db836c8cced524b1a2c4
Max Kästner <st172096@stud.uni-stuttgart.de>
2023-02-01 18:08:04 +0100

    fix statistic loading by removing ref
    Co-authored-by: programminghoch10 <16062290+programminghoch10@users.noreply.github.com>

--- src/components/MinigameStatisticComponents/FiniteQuizStatisticBox.vue
+++ src/components/MinigameStatisticComponents/FiniteQuizStatisticBox.vue
@@ -36,9 +36,7 @@ watch(
 
 async function loadMinigameStatistic(configurationId: string | undefined) {
   if (!configurationId) {
-    statistics.forEach(
-      (statistic) => (statistic.value.enoughDataToShow = false)
-    );
+    statistics.forEach((statistic) => (statistic.enoughDataToShow = false));
     throw new Error(
       "Configuration ID must be present when loading minigame specific statistics"
     );
@@ -47,18 +45,18 @@ async function loadMinigameStatistic(configurationId: string | undefined) {
   const promisesToWait = [
     loadTimeSpentDistributionInRangeBar(
       configurationId,
-      timeSpentDistributionRangeBar.value
+      timeSpentDistributionRangeBar
     ),
     loadProblematicQuestionsInBarChart(
       configurationId,
-      problematicQuestionsBarChart.value
+      problematicQuestionsBarChart
     ),
   ];
   await Promise.all(promisesToWait);
   loadingMinigameSpecificStatistics.value = false;
 }
 
-const timeSpentDistributionRangeBar = ref({
+const timeSpentDistributionRangeBar = {
   enoughDataToShow: true,
   width: 600,
   options: {
@@ -68,9 +66,9 @@ const timeSpentDistributionRangeBar = ref({
   series: [] as Array<{
     data: Array<{ x: string; y: Array<number> }>;
   }>,
-});
+};
 
-const problematicQuestionsBarChart = ref({
+const problematicQuestionsBarChart = {
   enoughDataToShow: true,
   width: 600,
   options: {
@@ -78,19 +76,17 @@ const problematicQuestionsBarChart = ref({
     title: { text: "Problematic questions per minigame run" },
   } as ApexOptions,
   series: [] as Array<{ name: string; data: Array<number> }>,
-});
+};
 
 const statistics = [
   timeSpentDistributionRangeBar,
   problematicQuestionsBarChart,
-] as Array<
-  Ref<{
-    enoughDataToShow: boolean;
-    width: number;
-    options: ApexOptions;
-    series: Array<any>;
-  }>
->;
+] as Array<{
+  enoughDataToShow: boolean;
+  width: number;
+  options: ApexOptions;
+  series: Array<any>;
+}>;
 
 loadMinigameStatistic(props.configurationId);
 </script>
@@ -99,15 +95,12 @@ loadMinigameStatistic(props.configurationId);
   <b-overlay :show="loadingMinigameSpecificStatistics" rounded="sm">
     <b-row id="minigame-specific-statistics" class="container mt-4">
       <h2>Minigame specific statistics</h2>
-      <b-col
-        v-for="statistic of statistics"
-        :key="statistic.value.options.title"
-      >
-        <b-overlay :show="!statistic.value.enoughDataToShow" rounded="sm">
+      <b-col v-for="statistic of statistics" :key="statistic.options.title">
+        <b-overlay :show="!statistic.enoughDataToShow" rounded="sm">
           <apexchart
-            :width="statistic.value.width"
-            :options="statistic.value.options"
-            :series="statistic.value.series"
+            :width="statistic.width"
+            :options="statistic.options"
+            :series="statistic.series"
           ></apexchart>
           <template #overlay>
             <h6>Statistic has not enough data to show.</h6>
