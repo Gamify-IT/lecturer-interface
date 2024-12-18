0dc887a7cd5f12a740217da6c6693ca79b6cbe07
Fabian <fabian.kalitzke@arcor.de>
2024-12-18 17:12:47 +0100

    changed to blob

--- src/ts/rest-clients/image-rest-client.ts
+++ src/ts/rest-clients/image-rest-client.ts
@@ -3,7 +3,7 @@ import config from "@/config";
 
 export async function postMemoryImage(
   uuid: string,
-  image: File
+  image: Blob
 ): Promise<AxiosResponse> {
   const formData = new FormData();
   formData.append("uuid", uuid);
@@ -12,8 +12,7 @@ export async function postMemoryImage(
   return axios.post(`${config.memoryApiUrl}/configurations/images`, formData, {
     headers: {
       "Content-Type": "multipart/formdata",
-    },
-  });
+    }});
 }
 
 export async function postFinitequizImage(
